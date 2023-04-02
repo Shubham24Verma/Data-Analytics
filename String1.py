5cadf5d140b3840d7e7ea0422d70322bc894e0e9
Shubham Verma <128981653+Shubham24Verma@users.noreply.github.com>
2023-04-02 22:13:02 +0530

    Add files via upload

---
+++ String1.py
@@ -0,0 +1,8 @@
+# seperator sep=
+# end operator end=
+# comment
+# variable
+
+print('Hi Everyone'); print('My name is Shubham Verma, how are you all ?')
+print('Hi, my name is shubham',end=' ')
+print('I am a Data Analyst')
